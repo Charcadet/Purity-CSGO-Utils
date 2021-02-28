5e72356066333b73547bfd8fbb739650733fca13
Rico <rico@live.it>
2021-02-28 17:35:39 +0100

    Update Offsets.cs for new CS:GO Update
    2/28/2021 CS:GO Game update. Fixed broken offsets.

--- riGO/Offsets.cs
+++ riGO/Offsets.cs
@@ -8,11 +8,11 @@ namespace riGO
 {
     public static class Offsets
     {
-        // UPDATED OFFSETS AT: 2/24/2021
+        // UPDATED OFFSETS AT: 2/28/2021
         public static Int32 dwClientState = 0x58EFE4;
-        public static Int32 dwLocalPlayer = 0xD8B2BC;
-        public static Int32 dwGlowObjectManager = 0x52EB540;
-        public static Int32 dwEntityList = 0x4DA2F44;
+        public static Int32 dwLocalPlayer = 0xD8B2CC;
+        public static Int32 dwGlowObjectManager = 0x52EB550;
+        public static Int32 dwEntityList = 0x4DA2F34;
 
         public static Int32 m_iHealth = 0x100;
         public static Int32 m_iGlowIndex = 0xA438;
@@ -20,7 +20,7 @@ namespace riGO
         public static Int32 m_bSpottedByMask = 0x980;
         public static Int32 m_flFlashDuration = 0xA420;
         public const Int32 m_fFlags = 0x104;
-        public const Int32 dwForceJump = 0x524CEA4;
+        public const Int32 dwForceJump = 0x524CE94;
         public const Int32 m_flFlashMaxAlpha = 0xA41C;
         public const Int32 m_iTeamNum = 0xF4;
         public const Int32 m_bDormant = 0xED;
