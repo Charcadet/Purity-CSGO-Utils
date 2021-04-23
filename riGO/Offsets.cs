c0ac2d3347472331be31670236e75e4e736cc004
Rico <ricoz@riseup.net>
2021-04-23 21:13:11 +0200

    Update Offsets.cs
    // UPDATED OFFSETS AT: 4/23/2021

--- riGO/Offsets.cs
+++ riGO/Offsets.cs
@@ -8,11 +8,11 @@ namespace riGO
 {
     public static class Offsets
     {
-        // UPDATED OFFSETS AT: 2/28/2021
-        public static Int32 dwClientState = 0x58EFE4;
-        public static Int32 dwLocalPlayer = 0xD8B2CC;
-        public static Int32 dwGlowObjectManager = 0x52EB550;
-        public static Int32 dwEntityList = 0x4DA2F34;
+        // UPDATED OFFSETS AT: 4/23/2021
+        public static Int32 dwClientState = 0x588FE4;
+        public static Int32 dwLocalPlayer = 0xD8C2BC;
+        public static Int32 dwGlowObjectManager = 0x52EC580;
+        public static Int32 dwEntityList = 0x4DA3F9C;
 
         public static Int32 m_iHealth = 0x100;
         public static Int32 m_iGlowIndex = 0xA438;
@@ -20,7 +20,7 @@ namespace riGO
         public static Int32 m_bSpottedByMask = 0x980;
         public static Int32 m_flFlashDuration = 0xA420;
         public const Int32 m_fFlags = 0x104;
-        public const Int32 dwForceJump = 0x524CE94;
+        public const Int32 dwForceJump = 0x524DEDC;
         public const Int32 m_flFlashMaxAlpha = 0xA41C;
         public const Int32 m_iTeamNum = 0xF4;
         public const Int32 m_bDormant = 0xED;
